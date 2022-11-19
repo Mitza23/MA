ba1f9f44597fb325b212fa6957602f4d3afdbd90
Mihai <mitzagrebla@yahoo.com>
2022-11-19 09:54:58 +0200

    A file was deleted

--- app/src/main/java/com/plcoding/mvvmtodoapp/SongApp.kt
+++
@@ -1,8 +0,0 @@
-package com.plcoding.mvvmtodoapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SongApp : Application(){
-}
--- app/src/main/java/com/plcoding/mvvmtodoapp/util/Routes.kt
+++
@@ -1,6 +0,0 @@
-package com.plcoding.mvvmtodoapp.util
-
-object Routes {
-    const val SONG_LIST = "song_list"
-    const val ADD_EDIT_SONG = "add_edit_song"
-}
